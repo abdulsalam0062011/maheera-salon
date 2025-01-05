8778c089f7b42b1b0c4686310f8482189118269d
abdulsalam0062011 <abdussalamsuleiman006@gmail.com>
2025-01-05 17:37:27 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,5 +1,1 @@
-// JavaScript for form submission (optional functionality)
-document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault(); // Prevent actual form submission
-  alert("Thank you for contacting Maheera Salon & Makeup!");
-});
+
